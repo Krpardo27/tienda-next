a3634801338353e0e9f8cfb9f4faaf31c9c22d2e
Krpardo27 <kevin.pardov26@gmail.com>
2026-04-07 23:09:51 -0400

    Se reinstala carpeta .next por caché y se elimina hok de tanksquery

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -5,7 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 export function useProducts(category: string) {
   return useQuery({
     queryKey: ["products", category],
-    queryFn: () => fetchProducts(category),
+    // queryFn: () => fetchProducts(category),
     staleTime: 1000 * 60 * 5, // 🔥 cache 5 min
   });
 }
